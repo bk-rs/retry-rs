ca0f6923a21c2cff8ae77c27e8103e25287ee3f9
vkill <vkill.net@gmail.com>
2022-02-10 14:19:05 +0800

    Update retry-policy

--- retry-policy/src/policies/google_cloud_workflows/http.rs
+++ retry-policy/src/policies/google_cloud_workflows/http.rs
@@ -2,17 +2,23 @@ use core::fmt;
 
 use retry_backoff::backoffs::google_cloud_workflows::Backoff;
 
+use crate::predicate::FnPredicate;
+
 use super::Policy;
 
 /// [Object: http.default_retry](https://cloud.google.com/workflows/docs/reference/stdlib/http/default_retry)
 pub fn default_retry() -> Policy<Error> {
-    Policy::new(default_retry_predicate, 5, Backoff::default())
+    Policy::new(
+        FnPredicate::from(default_retry_predicate),
+        5,
+        Backoff::default(),
+    )
 }
 
 /// [Object: http.default_retry_non_idempotent](https://cloud.google.com/workflows/docs/reference/stdlib/http/default_retry_non_idempotent)
 pub fn default_retry_non_idempotent() -> Policy<Error> {
     Policy::new(
-        default_retry_predicate_non_idempotent,
+        FnPredicate::from(default_retry_predicate_non_idempotent),
         5,
         Backoff::default(),
     )
@@ -65,7 +71,7 @@ pub enum Error {
     GatewayTimeout,
     ConnectionError,
     TimeoutError,
-    Other(Box<dyn std::error::Error + Send + Sync + 'static>),
+    Other,
 }
 
 impl fmt::Display for Error {
@@ -95,12 +101,11 @@ mod tests {
             Error::ConnectionError,
             Error::TimeoutError,
         ] {
-            assert!((policy.predicate)(err));
+            assert!(policy.predicate.require_retry(err));
         }
-        for err in &[Error::Other(Box::new(std::io::Error::from(
-            std::io::ErrorKind::TimedOut,
-        )))] {
-            assert!(!(policy.predicate)(err));
+        #[allow(clippy::single_element_loop)]
+        for err in &[Error::Other] {
+            assert!(!policy.predicate.require_retry(err));
         }
         assert_eq!(policy.max_retries, 5);
         assert_eq!(policy.backoff, Backoff::default());
@@ -117,16 +122,16 @@ mod tests {
                 retry_after_delay_seconds: None,
             },
         ] {
-            assert!((policy.predicate)(err));
+            assert!(policy.predicate.require_retry(err));
         }
         for err in &[
             Error::BadGateway,
             Error::GatewayTimeout,
             Error::ConnectionError,
             Error::TimeoutError,
-            Error::Other(Box::new(std::io::Error::from(std::io::ErrorKind::TimedOut))),
+            Error::Other,
         ] {
-            assert!(!(policy.predicate)(err));
+            assert!(!policy.predicate.require_retry(err));
         }
         assert_eq!(policy.max_retries, 5);
         assert_eq!(policy.backoff, Backoff::default());
--- retry-policy/src/policies/google_cloud_workflows/mod.rs
+++ retry-policy/src/policies/google_cloud_workflows/mod.rs
@@ -1,34 +1,25 @@
 //! [Retry steps](https://cloud.google.com/workflows/docs/reference/syntax/retrying)
 //!
 
-use core::fmt;
-
 use retry_backoff::backoffs::google_cloud_workflows::Backoff;
+
+use crate::predicate::Predicate;
 
 pub mod http;
 
 //
+#[derive(Debug)]
 pub struct Policy<E> {
     /// returns true if a retry; false otherwise
-    pub predicate: Box<dyn Fn(&E) -> bool>,
+    pub predicate: Box<dyn Predicate<E> + Send + Sync>,
     pub max_retries: usize,
     pub backoff: Backoff,
 }
 
-impl<E> fmt::Debug for Policy<E> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("Policy")
-            .field("predicate", &"")
-            .field("max_retries", &self.max_retries)
-            .field("backoff", &self.backoff)
-            .finish()
-    }
-}
-
 impl<E> Policy<E> {
-    pub fn new<F>(predicate: F, max_retries: usize, backoff: Backoff) -> Self
+    pub fn new<P>(predicate: P, max_retries: usize, backoff: Backoff) -> Self
     where
-        F: Fn(&E) -> bool + 'static,
+        P: Predicate<E> + Send + Sync + 'static,
     {
         Self {
             predicate: Box::new(predicate),
